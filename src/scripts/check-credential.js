a96eed88d07ffeb58bb5b4e7d40bd71dd9e3f56b
achibulup <achibulup@gmail.com>
2023-05-31 00:54:56 +0700

    remove check-credential.js

--- src/scripts/check-credential.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function checkCredential(username, password) {
-    if (username === 'admin' && password === 'admin') {
-        return 'success';
-    }
-    else {
-        return 'invalid username or password';
-    }
-}
