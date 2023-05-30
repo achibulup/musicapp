197c88578797a91731c2e9837eb13611b642631f
achibulup <achibulup@gmail.com>
2023-05-31 00:32:52 +0700

    uploaded song now appear on uploaded songs section too

--- src/scripts/songs-actions.ts
+++ src/scripts/songs-actions.ts
@@ -12,6 +12,12 @@ function addSong(song: SongData) : Song {
     ...song
   };
   SongList.push(newSong);
+  const songElement = makeSongElement(newSong);
+  const recentSongsElement = nonNull(document.getElementById('recent-songs'));
+  recentSongsElement.appendChild(songElement);
+  const songElement2 = makeSongElement(newSong);
+  const uploadedSongsElement = nonNull(document.getElementById('uploaded-songs'));
+  uploadedSongsElement.appendChild(songElement2);
   return newSong;
 }
 
